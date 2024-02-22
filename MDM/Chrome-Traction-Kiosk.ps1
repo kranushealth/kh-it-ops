effb5c259c79d1d1cbea621f317d2adfbd3beb0f
Marcel Thriemer <106150958+Leitwolf90@users.noreply.github.com>
2024-02-22 13:52:54 +0100

    Delete MDM/Chrome-Traction-Kiosk.ps1

--- MDM/Chrome-Traction-Kiosk.ps1
+++
@@ -1,1 +0,0 @@
-Start "C:\Program Files\Google\Chrome\Application\chrome.exe" '--start-fullscreen "http://app.powerbi.com/groups/59b521f2-7e55-437c-b042-815ee64c826b/reports/445643e5-f415-4cc6-aa1f-53bff4acce72/ReportSection?experience=power-bi&collapseNavigation=true&chromeless=true"'

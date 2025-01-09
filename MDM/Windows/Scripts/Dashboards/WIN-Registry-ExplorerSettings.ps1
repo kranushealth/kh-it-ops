5f2ca89c16703d9698bf7aba8092237143d6fb70
Marcel Thriemer <106150958+mthriemer@users.noreply.github.com>
2025-01-09 15:24:22 +0100

    Delete MDM/Windows/Scripts/Dashboards/WIN-Registry-ExplorerSettings.ps1

--- MDM/Windows/Scripts/Dashboards/WIN-Registry-ExplorerSettings.ps1
+++
@@ -1,9 +0,0 @@
-Function Set-ExplorerSettings {
-
-Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -value 0 #Show file extensions
-Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'LaunchTo' -value 1 #Open file explorer to This PC
-Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'AutoCheckSelect' -value 0 #Do not show item checkboxes
-
-}
-
-Set-ExplorerSettings
